c10818f01a3dbaaec3c58f6cd30e9e365639377b
Alexandra Mallet <malletalexandra@gmail.com>
2023-04-13 15:25:14 +0200

    commit pour push pour merge

--- src/user/Main.java
+++ src/user/Main.java
@@ -1,10 +1,23 @@
 package user;
 
+import java.sql.SQLException;
+
+import dao.DaoAuthentification;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-
+		test1();
+	}
+	
+	static void test1() throws ClassNotFoundException, SQLException
+	{
+		
+		if(DaoAuthentification.selectById("Med1", "psw"))
+			System.out.println("connecté");
+		else
+			System.out.println("échec connexion");
 	}
 
 }
